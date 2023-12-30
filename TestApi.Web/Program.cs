7c7e06ea391c95ba113c5096c25bb4d3c3455095
lieken1225 <lieken1225@gmail.com>
2023-12-31 03:13:37 +0800

    add DI

--- TestApi.Web/Program.cs
+++ TestApi.Web/Program.cs
@@ -1,7 +1,10 @@
+using TestApi.Web.Service.Interface;
+using TestApi.Web.Service.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddTransient<IDatabaseService, DatabaseService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,3 +26,8 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+//AddSingleton() : 顧名思義，AddSingleton()方法建立一個 Singleton 服務。 首次要求時會建立 Singleton 服務。 然後，所有後續的請求中都會使用相同的實例。 因此，通常，每個應用程式只建立一次 Singleton 服務，並且在整個應用程式生命週期中使用該單一實例。
+//AddTransient() :此方法會建立一個 Transient 服務。 每次請求時，都會建立一個新的 Transient 服務實例。
+//AddScoped() - 此方法建立一個 Scoped 服務。 在範圍內的每個請求中建立一個新的 Scoped 服務實例。 例如，在 Web 應用程式中，它為每個 http 請求建立 1 個實例，但在相同 Web 請求中的其他服務在呼叫這個請求的時候，都會使用相同的實例。 注意，它在一個客戶端請求中是相同的，但在多個客戶端請求中是不同的。
